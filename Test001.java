f7e84ca489da7365e67fd0a575d26cd2b44d8ccc
navaneetha1019 <navaneethanavi1010@gmail.com>
2025-01-22 20:34:41 +0530

    TEST001

---
+++ Test001.java
@@ -0,0 +1,10 @@
+public class Test001 {
+    public static void main(String[] args) {
+        int n = 11;
+        for (int i = 1; i <n; i++) {
+            if (i != 5 && i != 7) {
+                System.out.println(i);
+            }
+        }
+    }
+}
