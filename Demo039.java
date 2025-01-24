ba02e27a28f0a0671be4592f6bd2ad2b8a720ef2
navaneetha1019 <navaneethanavi1010@gmail.com>
2025-01-24 12:51:52 +0530

    check character lower case or upper case digit

---
+++ Demo039.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo039 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char ch = scanner.next().charAt(0);
+
+        if (Character.isLowerCase(ch)) {
+            System.out.println("Lowercase");
+        } else if (Character.isUpperCase(ch)) {
+            System.out.println("Uppercase");
+        } else if (Character.isDigit(ch)) {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Other");
+        }
+    }
+}
+
