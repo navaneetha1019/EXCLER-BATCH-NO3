f4e0f5bcd2291fbfe0f5d42d9c03cc9c8672956e
navaneetha1019 <navaneethanavi1010@gmail.com>
2025-01-26 11:02:34 +0530

    greatest among 2 numbers

---
+++ Demo002.java
@@ -0,0 +1,11 @@
+public class Demo002 {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 45;
+        if (a > b) {
+            System.out.println("Greater number is A: " + a);
+        } else {
+            System.out.println("Greater number is B: " + b);
+        }
+    }
+}
